eaad56d4d09c99bf7ea15bf750b15dfed2c3e48d
Chi Tse <chiyantsestudio@gmail.com>
2022-06-26 15:33:18 -0400

    finish code for exercises 29.7.2 Part1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'random cute pet photos';
+  image1 = 'http://images2.fanpop.com/image/answers/342000/342741_1263941970327.98res_449_393.jpg';
+  image2 = 'https://i.redd.it/jvostfk4v9r71.jpg';
+  image3 = 'https://i.pinimg.com/originals/cf/4a/19/cf4a19637df4dd74d04f913f3ecea65f.jpg';
 
   constructor() { }
 
